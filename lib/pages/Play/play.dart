59e734c7a8441a52e5628860fbec80f06de7e457
xin.peng <xin.peng@BKJK.COM>
2019-02-13 16:17:24 +0800

    do some optimization

--- lib/pages/Play/play.dart
+++ lib/pages/Play/play.dart
@@ -16,30 +16,17 @@ class PlayState extends State<Play> with SingleTickerProviderStateMixin{
 
   dynamic songDetail;
   bool initPlay;
-  AnimationController coverController;
   CurvedAnimation coverCurved;
 
   @override
   void initState() {
     super.initState();
     initPlay = false;
-    coverController = new AnimationController(
-      vsync: this,
-      duration: Duration(
-        seconds: 40
-      ),
-    );
-    coverCurved = new CurvedAnimation(
-      parent: coverController,
-      curve: Curves.linear
-    );
-    coverController.repeat();
   }
 
   @override
   void dispose() {
     if(this.mounted) {
-      coverController.dispose();
       super.dispose();
     }
   }
@@ -75,162 +62,144 @@ class PlayState extends State<Play> with SingleTickerProviderStateMixin{
                 )
               ),
               Container(
-                height: 100,
-                margin: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.width,
+                child: CachedNetworkImage(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.width,
+                  imageUrl: state.playControllerState.playList[state.playControllerState.currentIndex - 1]['al']['picUrl'],
+                  placeholder: Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.width,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+              Container(
+                height: 70,
+                margin: EdgeInsets.only(top: MediaQuery.of(context).size.width - 70),
+                padding: EdgeInsets.only(left: 40),
+                alignment: Alignment.centerLeft,
+                decoration: BoxDecoration(
+                  color: Colors.black26
+                ),
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisSize: MainAxisSize.max,
                   children: <Widget>[
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pop(context);
-                      },
-                      child: Icon(
-                        Icons.arrow_back,
-                        color: Colors.white,
-                      )
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          state.playControllerState.playList[state.playControllerState.currentIndex - 1]['name'],
-                          style: TextStyle(
-                            fontSize: 17,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white
+                    Container(
+                      width: (MediaQuery.of(context).size.width - 50) * 0.6,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            state.playControllerState.playList[state.playControllerState.currentIndex - 1]['name'],
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20
+                            ),
+                            textAlign: TextAlign.left,
                           ),
-                        ),
-                        Text(
-                          state.playControllerState.playList[state.playControllerState.currentIndex - 1]['ar'][0]['name'],
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: Colors.white
-                          ),
-                        )
-                      ],
+                          Text(
+                            state.playControllerState.playList[state.playControllerState.currentIndex - 1]['ar'][0]['name'],
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15
+                            ),
+                            textAlign: TextAlign.left,
+                          )
+                        ],
+                      ),
                     ),
-                    GestureDetector(
-                      onTap: () {
-                        print('暂未开发');
-                      },
-                      child: Icon(
-                        Icons.more_horiz,
-                        color: Colors.white,
+                    Container(
+                      width: (MediaQuery.of(context).size.width - 50) * 0.4,
+                      child: Row(
+                        children: <Widget>[
+                          IconButton(
+                            iconSize: 20,
+                            color: Colors.white,
+                            onPressed: () {
+                              print('未开发');
+                            },
+                            icon: ImageIcon(
+                              AssetImage(
+                                'assets/images/loop.png'
+                              )
+                            )
+                          ),
+                          IconButton(
+                            iconSize: 20,
+                            color: Colors.white,
+                            onPressed: () {
+                              print('未开发');
+                            },
+                            icon: ImageIcon(
+                              AssetImage(
+                                'assets/images/random.png'
+                              )
+                            )
+                          )
+                        ],
                       )
                     )
                   ],
                 )
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Stack(
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.3),
-                        width: MediaQuery.of(context).size.width * 0.6,
-                        height: MediaQuery.of(context).size.width * 0.6,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.black12,
-                          boxShadow: <BoxShadow>[
-                            BoxShadow(
-                              color: Colors.black26,
-                              blurRadius: 1,
-                              spreadRadius: 1,
-                              offset: Offset(0, 0)
-                            )
-                          ]
-                        ),
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width * 0.6 - 20,
-                        height: MediaQuery.of(context).size.width * 0.6 - 20,
-                        margin: EdgeInsets.fromLTRB(10, MediaQuery.of(context).size.height * 0.3 + 10, 0, 0),
-                        child: ClipOval(
-                          child: 
-                          state.playControllerState.playing || (this.initPlay != null && this.initPlay == true)
-                          ?
-                          RotationTransition(
-                            turns: coverCurved,
-                            child: CachedNetworkImage(
-                              imageUrl: state.playControllerState.playList[state.playControllerState.currentIndex - 1]['al']['picUrl'],
-                              placeholder: Container(
-                                width: MediaQuery.of(context).size.width * 0.6 - 20,
-                                height: MediaQuery.of(context).size.width * 0.6 - 20,
-                                color: Colors.grey,
-                              ),
-                            ),
-                          )
-                          :
-                          CachedNetworkImage(
-                            imageUrl: state.playControllerState.playList[state.playControllerState.currentIndex - 1]['al']['picUrl'],
-                            placeholder: Container(
-                              width: MediaQuery.of(context).size.width * 0.6 - 20,
-                              height: MediaQuery.of(context).size.width * 0.6 - 20,
-                              color: Colors.grey,
-                            ),
-                          ),
-                        )
-                      ),
-                    ],
-                  )
-                ],
-              ),
-              Container(
-                margin: EdgeInsets.fromLTRB(40, MediaQuery.of(context).size.height * 0.7, 40, 0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    IconButton(
-                      onPressed: () {
-                        print('暂未开发');
-                      },
-                      color: Colors.white,
-                      iconSize: 20,
-                      icon: ImageIcon(
-                        AssetImage(
-                          'assets/images/like_playList.png',
-                        )
-                      ),
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        print('暂未开发');
-                      },
-                      color: Colors.white,
-                      iconSize: 20,
-                      icon: ImageIcon(
-                        AssetImage(
-                          'assets/images/comment_playList.png',
-                        )
-                      ),
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        print('暂未开发');
-                      },
-                      color: Colors.white,
-                      iconSize: 20,
-                      icon: ImageIcon(
-                        AssetImage(
-                          'assets/images/retweet_playList.png',
-                        )
-                      ),
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        print('暂未开发');
-                      },
-                      color: Colors.white,
-                      iconSize: 20,
-                      icon: Icon(Icons.file_download)
-                    )
-                  ],
-                ),
-              ),
-              PlayController(songId, state, coverController, setInitPlay)
+              // Container(
+              //   margin: EdgeInsets.fromLTRB(40, MediaQuery.of(context).size.height * 0.7, 40, 0),
+              //   child: Row(
+              //     mainAxisAlignment: MainAxisAlignment.spaceAround,
+              //     children: <Widget>[
+              //       IconButton(
+              //         onPressed: () {
+              //           print('暂未开发');
+              //         },
+              //         color: Colors.white,
+              //         iconSize: 20,
+              //         icon: ImageIcon(
+              //           AssetImage(
+              //             'assets/images/like_playList.png',
+              //           )
+              //         ),
+              //       ),
+              //       IconButton(
+              //         onPressed: () {
+              //           print('暂未开发');
+              //         },
+              //         color: Colors.white,
+              //         iconSize: 20,
+              //         icon: ImageIcon(
+              //           AssetImage(
+              //             'assets/images/comment_playList.png',
+              //           )
+              //         ),
+              //       ),
+              //       IconButton(
+              //         onPressed: () {
+              //           print('暂未开发');
+              //         },
+              //         color: Colors.white,
+              //         iconSize: 20,
+              //         icon: ImageIcon(
+              //           AssetImage(
+              //             'assets/images/retweet_playList.png',
+              //           )
+              //         ),
+              //       ),
+              //       IconButton(
+              //         onPressed: () {
+              //           print('暂未开发');
+              //         },
+              //         color: Colors.white,
+              //         iconSize: 20,
+              //         icon: Icon(Icons.file_download)
+              //       )
+              //     ],
+              //   ),
+              // ),
+              PlayController(songId, state, setInitPlay)
             ],
           )
         );
@@ -242,9 +211,8 @@ class PlayState extends State<Play> with SingleTickerProviderStateMixin{
 class PlayController extends StatelessWidget {
   int songId;
   dynamic state;
-  AnimationController coverController;
   dynamic setInitPlay;
-  PlayController(this.songId, this.state, this.coverController, this.setInitPlay);
+  PlayController(this.songId, this.state, this.setInitPlay);
 
   @override
   Widget build(BuildContext context) {
@@ -259,18 +227,6 @@ class PlayController extends StatelessWidget {
               width: 50,
               height: 50,
               padding: EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                color: Colors.grey[900],
-                shape: BoxShape.circle,
-                boxShadow: <BoxShadow>[
-                  BoxShadow(
-                    color: Colors.black12,
-                    blurRadius: 1,
-                    spreadRadius: 1,
-                    offset: Offset(0, -1)
-                  )
-                ]
-              ),
               child: Image.asset(
                 'assets/images/play_prev.png'
               )
@@ -288,10 +244,7 @@ class PlayController extends StatelessWidget {
               builder: (BuildContext context, callback) {
                 return GestureDetector(
                   onTap: () {
-                    if(state.playControllerState.playing == true) {
-                      this.coverController.stop();
-                    } else {
-                      this.coverController.forward();
+                    if(state.playControllerState.playing != true) {
                       this.setInitPlay(true);
                     }
                     callback();
@@ -300,18 +253,6 @@ class PlayController extends StatelessWidget {
                     width: 50,
                     height: 50,
                     padding: EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                      color: Colors.grey[900],
-                      shape: BoxShape.circle,
-                      boxShadow: <BoxShadow>[
-                        BoxShadow(
-                          color: Colors.black12,
-                          blurRadius: 1,
-                          spreadRadius: 1,
-                          offset: Offset(0, -1)
-                        )
-                      ]
-                    ),
                     child: state.playControllerState.playing
                     ?
                     Image.asset(
@@ -329,18 +270,6 @@ class PlayController extends StatelessWidget {
               width: 50,
               height: 50,
               padding: EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                color: Colors.grey[900],
-                shape: BoxShape.circle,
-                boxShadow: <BoxShadow>[
-                  BoxShadow(
-                    color: Colors.black12,
-                    blurRadius: 1,
-                    spreadRadius: 1,
-                    offset: Offset(0, -1)
-                  )
-                ]
-              ),
               child: Image.asset(
                 'assets/images/play_next.png'
               )
