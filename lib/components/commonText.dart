b39705e12fa95460f2c6e3d5e58267680aa9b756
xin.peng <xin.peng@BKJK.COM>
2020-01-17 14:04:54 +0800

    add commonText component

---
+++ lib/components/commonText.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CommonText extends StatelessWidget {
+  final String text;
+  final double fontSize;
+  final int maxLines;
+  final Color color;
+  final FontWeight fontWeight;
+
+  CommonText(this.text, this.fontSize, this.maxLines, this.color, this.fontWeight);
+
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      maxLines: maxLines,
+      overflow: TextOverflow.ellipsis,
+      style: TextStyle(
+        color: color,
+        fontSize: fontSize,
+        fontWeight: fontWeight
+      ),
+    );
+  }
+}
